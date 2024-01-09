cba35d6aea01793c688ec2a9f81e6ec7cf2db18f
StungelX <stungelx@gmail.com>
2024-01-09 20:44:30 +0200

    Fixed some trooble

--- Abstract_fabric_patern.py
+++ Abstract_fabric_patern.py
@@ -7,18 +7,18 @@ class AbstractFactory(ABC):
     def create_table(self) -> Table:
         pass
 
-    def some_operation_table(self) -> str:
+    def some_operation(self) -> str:
         product = self.create_table()
-        result = f"\nOperation finished. Product {product.useful_function_table()} creted."
+        result = f"Operation finished. Product {product.fabric()} creted."
         return result
 
     @abstractmethod
     def create_chair(self) -> Chair:
         pass
 
-    def some_operation_chair(self) -> str:
+    def some_operation1(self) -> str:
         product = self.create_chair()
-        result = f"\nOperation finished. Product {product.useful_function_chair()} creted."
+        result = f"Operation finished. Product {product.fabric()} creted."
         return result
 
 
@@ -28,9 +28,6 @@ class ConcreteFactory1(AbstractFactory):
 
     def create_chair(self) -> Chair:
         return ModernChair()
-
-    def operation(self) -> str:
-        pass
 
 
 class ConcreteFactory2(AbstractFactory):
@@ -46,15 +43,25 @@ class Table(ABC):
     def useful_function_table(self) -> str:
         pass
 
+    @abstractmethod
+    def fabric(self) -> str:
+        pass
+
 
 class ModernTable(Table):
     def useful_function_table(self) -> str:
+        return "The result of the product Modern Table"
+
+    def fabric(self) -> str:
         return "Modern Table"
 
 
 class VictorianTable(Table):
     def useful_function_table(self) -> str:
         return "The result of the product Victorian Table."
+
+    def fabric(self) -> str:
+        return "Victorian Table"
 
 
 class Chair(ABC):
@@ -66,14 +73,21 @@ class Chair(ABC):
     def another_useful_function_chair(self, collaborator: Table) -> None:
         pass
 
+    @abstractmethod
+    def fabric(self) -> str:
+        pass
+
 
 class ModernChair(Chair):
     def useful_function_chair(self) -> str:
-        return "Modern Chair."
+        return "The result of the product Modern Chair."
 
     def another_useful_function_chair(self, collaborator: Table) -> str:
-        result = collaborator.useful_function_table()
-        return f"Modern Chair collaborating with the ({result})"
+        result = collaborator.fabric()
+        return f"The result of the Modern Chair collaborating with the {result}"
+
+    def fabric(self) -> str:
+        return "Modern Chair"
 
 
 class VictorianChair(Chair):
@@ -81,18 +95,23 @@ class VictorianChair(Chair):
         return "The result of the product Victorian Chair."
 
     def another_useful_function_chair(self, collaborator: Table):
-        result = collaborator.useful_function_table()
-        return f"The result of the Victorian Chair collaborating with the ({result})"
+        result = collaborator.fabric()
+        return f"The result of the Victorian Chair collaborating with the {result}"
+
+    def fabric(self) -> str:
+        return "Victorian Chair"
 
 
 def client_code(factory: AbstractFactory) -> None:
     product_a = factory.create_table()
     product_b = factory.create_chair()
-    text_modern = factory.some_operation_table()
+    text_table = factory.some_operation()
+    text_chair = factory.some_operation1()
 
     print(f"{product_b.useful_function_chair()}")
-    print(f"{product_b.another_useful_function_chair(product_a)}", end="")
-    print(f"{text_modern}")
+    print(f"{text_chair}")
+    print(f"{text_table}")
+    print(f"{product_b.another_useful_function_chair(product_a)}")
 
 
 if __name__ == "__main__":
