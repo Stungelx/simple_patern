c426b84fc786c3d01bbd705b4ca37831060a36d0
StungelX <stungelx@gmail.com>
2024-01-23 23:38:46 +0200

    Add Prototype_patern

--- Prototype_patern.py
+++ Prototype_patern.py
@@ -9,108 +9,52 @@ class SelfReferencingEntity:
         self.parent = parent
 
 
-class SomeComponent:
-    def __init__(self, some_int, some_list_of_objects, some_circular_ref):
-        self.some_int = some_int
-        self.some_list_of_objects = some_list_of_objects
-        self.some_circular_ref = some_circular_ref
+class VictorianSet:
+    def __init__(self, tabletop_type, number_of_legs, number_of_chairs, lamination):
+        self.tabletop_type = tabletop_type
+        self.number_of_legs = number_of_legs
+        self.number_of_chairs = number_of_chairs
+        self.lamination = lamination
 
     def __copy__(self):
-        some_list_of_objects = copy.copy(self.some_list_of_objects)
-        some_circular_ref = copy.copy(self.some_circular_ref)
+        lamination = copy.copy(self.lamination)
 
-        new = self.__class__(
-            self.some_int, some_list_of_objects, some_circular_ref
-        )
+        new = self.__class__(self.tabletop_type, self.number_of_legs, self.number_of_chairs, lamination)
         new.__dict__.update(self.__dict__)
         return new
 
     def __deepcopy__(self, memo=None):
         if memo is None:
             memo = {}
-        some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
-        some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
 
-        new = self.__class__(
-            self.some_int, some_list_of_objects, some_circular_ref
-        )
+        lamination = copy.deepcopy(self.lamination, memo)
+
+        new = self.__class__(self.tabletop_type, self.number_of_legs, self.number_of_chairs, lamination)
         new.__dict__ = copy.deepcopy(self.__dict__, memo)
-
         return new
+
+    def __str__(self):
+        return f"tabletop_type={self.tabletop_type}, number_of_legs={self.number_of_legs}, " \
+               f"number_of_chairs={self.number_of_chairs}, lamination={self.lamination}"
 
 
 if __name__ == "__main__":
+    victorian_full_set = VictorianSet('tree', 3, 3, {'type': '3d'})
+    print(f"Create object victorian_full_set: ({victorian_full_set})")
+    victorian_full_set_copy = copy.copy(victorian_full_set)
+    print(f"Create object victorian_full_set_copy: ({victorian_full_set_copy})")
+    print("\n")
 
-    list_of_objects = [1, {1, 2, 3}, [1, 2, 3]]
-    circular_ref = SelfReferencingEntity()
-    component = SomeComponent(23, list_of_objects, circular_ref)
-    circular_ref.set_parent(component)
+    print("Add new key and value to dictionary 'lamination' in object victorian_full_set_copy, "
+          "and see, that change values in victorian_full_set too:")
+    victorian_full_set_copy.lamination.update({'color': 'brown'})
+    print(f"victorian_full_set_copy: ({victorian_full_set_copy})")
+    print(f"victorian_full_set: ({victorian_full_set})")
+    print("\n")
 
-    shallow_copied_component = copy.copy(component)
-    shallow_copied_component.some_list_of_objects.append("another object")
-    if component.some_list_of_objects[-1] == "another object":
-        print(
-            "Adding elements to `shallow_copied_component`'s "
-            "some_list_of_objects adds it to `component`'s "
-            "some_list_of_objects."
-        )
-    else:
-        print(
-            "Adding elements to `shallow_copied_component`'s "
-            "some_list_of_objects doesn't add it to `component`'s "
-            "some_list_of_objects."
-        )
-    component.some_list_of_objects[1].add(4)
-    if 4 in shallow_copied_component.some_list_of_objects[1]:
-        print(
-            "Changing objects in the `component`'s some_list_of_objects "
-            "changes that object in `shallow_copied_component`'s "
-            "some_list_of_objects."
-        )
-    else:
-        print(
-            "Changing objects in the `component`'s some_list_of_objects "
-            "doesn't change that object in `shallow_copied_component`'s "
-            "some_list_of_objects."
-        )
-
-    deep_copied_component = copy.deepcopy(component)
-    deep_copied_component.some_list_of_objects.append("one more object")
-    if component.some_list_of_objects[-1] == "one more object":
-        print(
-            "Adding elements to `deep_copied_component`'s "
-            "some_list_of_objects adds it to `component`'s "
-            "some_list_of_objects."
-        )
-    else:
-        print(
-            "Adding elements to `deep_copied_component`'s "
-            "some_list_of_objects doesn't add it to `component`'s "
-            "some_list_of_objects."
-        )
-
-    component.some_list_of_objects[1].add(10)
-    if 10 in deep_copied_component.some_list_of_objects[1]:
-        print(
-            "Changing objects in the `component`'s some_list_of_objects "
-            "changes that object in `deep_copied_component`'s "
-            "some_list_of_objects."
-        )
-    else:
-        print(
-            "Changing objects in the `component`'s some_list_of_objects "
-            "doesn't change that object in `deep_copied_component`'s "
-            "some_list_of_objects."
-        )
-
-    print(
-        f"id(deep_copied_component.some_circular_ref.parent): "
-        f"{id(deep_copied_component.some_circular_ref.parent)}"
-    )
-    print(
-        f"id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent): "
-        f"{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
-    )
-    print(
-        "^^ This shows that deepcopied objects contain same reference, they "
-        "are not cloned repeatedly."
+    print("But we can create new object with deepcopy, and add new key and value to dictionary 'lamination' in "
+          "victorian_full_set_deepcopy, and see, that values in victorian_full_set not update")
+    victorian_full_set_deepcopy = copy.deepcopy(victorian_full_set)
+    victorian_full_set_deepcopy.lamination.update({'caption': 'AVADA'})
+    print(f"victorian_full_set_deepcopy: ({victorian_full_set_deepcopy})")
+    print(f"victorian_full_set: ({victorian_full_set})")
